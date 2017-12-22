f946101c479a3bd16cab818ac79dd8b508e31a9f
mark <mark@vtista.com>
2017-12-22 17:48:27 +0800

    fix

--- config/index.js
+++ config/index.js
@@ -1,19 +1,5 @@
-
-/**
- * This file let you define your own necessary options for your appliaction.
- */
-
- // Simulate config options from your production environment by
-// customising the .env file in your project's root folder.
-require('dotenv').config();
-
-//You may have some more options need to set but can be fill by .env file
-//can set here.
-config ={
-    serverURL:process.env.host+":"+process.env.appPort+process.env.mountPath,
-    
-};
-
-config.env=process.env;
-module.exports=config;
-
+module.exports ={
+    serverPort:4040,
+    serverURL:"http://localhost:4040/parse",
+    databaseURI:"mongodb://localhost/blog"
+    };
--- index.js
+++ index.js
@@ -1,49 +1,15 @@
-/**
- * This file is your app entry.
- */
-
-
-
 var express = require('express');
-<<<<<<< HEAD
-
-//Load the app configs
 $config=require('./config');
-
-//load the app Helper
-$helper=require('./app/Helpers');
-
-//var path = require('path');
-=======
 var ParseServer =require('./server/Parse_Server');
 var ParseDashboard = require('./server/Parse_Dashboard');
->>>>>>> parent of c26fd8a... fulfill config/index.js with global settings
-
 
 var app = express();
-<<<<<<< HEAD
-
-//Load the routes
-require('./app/Routes')(app);
-
-
- //Initialise the web server
-var ParseHttpServer = require('http').createServer(app);
-ParseHttpServer.listen($config.env.appPort,function(){
-    console.log('parse-server parse-dashboard are  running on port %s.',$config.env.appPort);
-=======
 // 把 Parse Server 挂载在 /parse
 app.use('/parse', ParseServer);
 
 // 把 Parse Dashboard 挂载在 /dashboard
 app.use('/dashboard', ParseDashboard);
- 
 var httpServer = require('http').createServer(app);
-httpServer.listen(4040,function(){
-    console.log('parse-server-dashboard running on port 4040.');
->>>>>>> parent of c26fd8a... fulfill config/index.js with global settings
+httpServer.listen($config.serverPort,function(){
+console.log('parse-server parse-dashboard are running on port 4040.');
 });
-
-
-
-
